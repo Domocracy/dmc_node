721a2b54c683493d4d11339ed4be077242ab5ae3
technik <cjtech90@gmail.com>
2015-06-30 11:15:19 +0200

    Added test cases for many variations of protocol and host.

--- test/unitTest/frontEnd/requestDispatcher/main.cpp
+++ test/unitTest/frontEnd/requestDispatcher/main.cpp
@@ -7,6 +7,7 @@
 #define Net_HTTPRequest_INCLUDED // Deactivate inclusion of POCO HTTP Requests
 #include "frontEnd/RequestDispatcher.h"
 #include <assert.h>
+#include <vector>
 
 // Mock classes
 namespace Poco {
@@ -21,41 +22,60 @@ namespace Poco {
 } // namespace Poco
 
 namespace dmc {
-	class RequestProcessor {
-	public:
-		enum class Type {
-			eEventPublisher,
-			eCommandDispatcher
-		};
-
-		virtual Type iAm() const = 0;
-	};
-
-	class EventPublisher : public RequestProcessor {
-		Type iAm() const { return Type::eEventPublisher; }
-	};
-
-	class CommandDispatcher : public RequestProcessor {
-		Type iAm() const { return Type::eCommandDispatcher; }
-	};
+	class RequestProcessor {};
 } // namespace dmc
 
 using namespace Poco::Net;
 using namespace dmc;
+using namespace std;
 
 int main(int, const char**) {
-	RequestDispatcher dispatcher;
+	vector<string>	protocols = { "http", "https" };
+	vector<string>	hosts = { "localhost", "192.168.0.64" };
+	vector<string>	urlBases;
+	for(auto host : hosts)
+		for(auto protocol : protocols)
+			urlBases.push_back(protocol + "://"+ host + "/");
 
+	// Empty dispatcher should never retrieve anything
 	std::string outUrl;
-	// Test invalid requests
-	assert(nullptr == dispatcher.dispatch(HTTPRequest{""}, outUrl));
-	assert(nullptr == dispatcher.dispatch(HTTPRequest{"/suscribe/a"}, outUrl));
-	assert(nullptr == dispatcher.dispatch(HTTPRequest{"/command/b/c/d?=e"}, outUrl));
+	RequestDispatcher a;
+	assert(nullptr == a.dispatch({""}, outUrl));
+	assert(outUrl.empty());
+	assert(nullptr == a.dispatch({"/"}, outUrl));
+	assert(outUrl.empty());
+	assert(nullptr == a.dispatch({"/suscribe"}, outUrl));
+	assert(outUrl.empty());
+	for(auto base : urlBases) {
+		assert(nullptr == a.dispatch({base+"suscribe"}, outUrl));
+	}
 
-	// Test valid requests
-	RequestProcessor* a = dispatcher.dispatch(HTTPRequest{"http://some.host/suscribe/"}, outUrl);
-	assert(a->iAm() == RequestProcessor::Type::eEventPublisher);
-	assert(outUrl == "");
+	// Top level suscriber
+	RequestDispatcher b;
+	RequestProcessor foo;
+	b.suscribe(&foo, "/");
+	assert(nullptr == b.dispatch({""}, outUrl)); // Ill-formed
+	assert(outUrl.empty());
+	assert(nullptr == b.dispatch({"/"}, outUrl)); // Ill-formed
+	assert(outUrl.empty());
+	assert(nullptr == b.dispatch({"/suscribe"}, outUrl)); // Ill-formed
+	assert(outUrl.empty());
+	for(auto base : urlBases) {
+		assert(&foo == b.dispatch({base}, outUrl));
+	}
+	for(auto base : urlBases) {
+		assert(&foo == b.dispatch({base+"hello"}, outUrl));
+	}
+
+	// Two suscribers
+	RequestDispatcher c;
+	RequestProcessor topLevel, specific;
+	c.suscribe(&topLevel, "/top");
+	c.suscribe(&specific, "/top/spec");
+	assert(nullptr == c.dispatch({"/x"}, outUrl)); // No one suscribed here
+	assert(outUrl.empty());
+	assert(c.dispatch({urlBases[0] + "top/spec/what"}, outUrl) == &specific);
+	assert(outUrl == "top/spec/what");
 }
 
 
