8700a2b0675f1233a4c0a2da94322ecaada1407b
technik <cjtech90@gmail.com>
2015-07-01 15:56:16 +0200

    WIP: Unit test for local server.

--- test/unitTest/frontEnd/localServer/test.cpp
+++ test/unitTest/frontEnd/localServer/test.cpp
@@ -1,11 +1,59 @@
 //----------------------------------------------------------------------------------------------------------------------
 // local server unit test
 //----------------------------------------------------------------------------------------------------------------------
+#include <string>
+#include <frontEnd/LocalServer.h>
 
 // Mock classes
+namespace Poco {
+	namespace Net {
+		class HTTPRequest;
+	}
+}
+
+namespace dmc {
+	// --- Mock response
+	class Response {
+	public:
+	};
+
+	// --- Mock request processor
+	class RequestProcessor {
+	public:
+		void processRequest(const Request& _req, const std::string& _parsedUrl, LocalServer& _server) {
+			while(!mRespond)
+			{}
+			_server.respond(_req, response);
+			mRespond = false;
+		}
+
+		bool mRespond = false;
+		Response response;
+	};
+
+	RequestProcessor gA;
+
+	// --- Mock request dispatcher
+	class RequestDispatcher {
+	public:
+		RequestProcessor* dispatch(const Poco::Net::HTTPRequest &_request, std::string &_parsedUrl) const {
+			return &gA;
+		}
+	};
+}	// namespace dmc
+
+using namespace dmc;
 
 int main(int, const char**) {
-	// Test 
+	// --- Test setup ---
+	RequestDispatcher dispatcher;
+	LocalServer server(dispatcher, 5028);
+	// Test POCO systems were properly called during configuration
+	// --- Test listening ---
+	server.startListening();
+	// Test a request
+	// --- Test stop ---
+	server.stopListening();
 	return 0;
 }
 
