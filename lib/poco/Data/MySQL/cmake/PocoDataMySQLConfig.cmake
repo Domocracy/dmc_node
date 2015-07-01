326d4929d89c7cb1d6e1076c62fe736c3cd4c418
technik <cjtech90@gmail.com>
2015-07-01 13:05:37 +0200

    Complete clean-up for gh-pages.

--- lib/poco/Data/MySQL/cmake/PocoDataMySQLConfig.cmake
+++
@@ -1,5 +0,0 @@
-include(CMakeFindDependencyMacro)
-set(CMAKE_PREFIX_PATH ${CMAKE_CURRENT_LIST_DIR})
-find_dependency(PocoFoundation)
-find_dependency(PocoData)
-include("${CMAKE_CURRENT_LIST_DIR}/PocoDataMySQLTargets.cmake")
