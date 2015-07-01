578d19238433cb1f7b843349e2d95680718f6669
technik <cjtech90@gmail.com>
2015-07-01 14:08:58 +0200

    Candidate interface for local server.

--- src/code/frontEnd/LocalServer.h
+++ src/code/frontEnd/LocalServer.h
@@ -6,7 +6,24 @@
 
 namespace dmc {
 
+	class RequestDispatcher;
+	class Request;
+	class Response;
+
 	class LocalServer {
+		/// \param dispatcher Local server will use this to retrieve the proper RequestProcessor for each incomming Request
+		/// \param port the server will try to listen on
+		LocalServer(RequestDispatcher& dispatcher, unsigned port);
+		// Listening interface
+		bool startListening	();
+		bool stopListening	();
+
+		// Ongoing connections
+		/// Respond to a pending request
+		///\ param request the request to which you intend to respond
+		///\ param response the response you want to send
+		///\ return \c true on success, \c false otherwise
+		bool respond(const Request& request, const Response& response);
 	};
 
 }	//	namespace dmc
