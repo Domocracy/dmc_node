d3ace90b840bebe8f6bec4ee95b450bdec76c180
technik <cjtech90@gmail.com>
2015-07-06 20:45:35 +0200

    Created dummy httpTranslator test.

---
+++ test/unitTest/frontEnd/httpTranslator/test.cpp
@@ -0,0 +1,29 @@
+//----------------------------------------------------------------------------------------------------------------------
+// local server unit test
+//----------------------------------------------------------------------------------------------------------------------
+#include <cassert>
+#include <string>
+#include <frontEnd/HttpTranslator.h>
+#include <frontEnd/Request.h>
+#include <frontEnd/Response.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+
+namespace dmc {
+	// --- Mock
+}	// namespace dmc
+
+using namespace dmc;
+using namespace Poco::Net;
+using namespace std;
+
+int main(int, const char**) {
+	// --- Test setup ---
+	return 0;
+}
+
+
+
+
+
