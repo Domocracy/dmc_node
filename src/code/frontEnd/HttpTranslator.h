6cf6d713d48766e8914c1eb0d45fb5ce0ba7cf6f
technik <cjtech90@gmail.com>
2015-07-06 19:55:10 +0200

    Doxygen documentation for HTTP->DMC translate.

--- src/code/frontEnd/HttpTranslator.h
+++ src/code/frontEnd/HttpTranslator.h
@@ -18,6 +18,10 @@ namespace dmc {
 
 	class HTTPTranslator {
 	public:
+		/// Translate and incomming HTTPRequest into a dmc::Request.
+		/// If the incomming request is ill-formed (for example with an incomplete Json as a body)
+		/// Translation will fail, the function will return false, and the contents of
+		/// the destination request will be undefined
 		/// \param _id this will be the id assigned to the resulting request
 		bool translate(const Poco::Net::HTTPServerRequest&, Request&);
 		bool translate(const Response&, Poco::Net::HTTPServerResponse&);
