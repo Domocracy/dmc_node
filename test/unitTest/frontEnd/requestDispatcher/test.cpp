2be7d93342c77b645baa9bb19e1e7683704d8315
Bardo91 <19Bardo91@gmail.com>
2015-07-06 11:43:22 +0200

    Simple mock classes of Request, Response, LocalServer and RequestProcessor to test RequestDispatcher.

--- test/unitTest/frontEnd/requestDispatcher/test.cpp
+++ test/unitTest/frontEnd/requestDispatcher/test.cpp
@@ -13,7 +13,23 @@
 
 // Mock classes
 namespace dmc {
-	class RequestProcessor {};
+	class Request {};
+	class Response {};
+	class LocalServer {
+	public:
+		bool respond(const Request &_request, const Response &_response) {
+			return true;
+		}
+	};
+
+	class RequestProcessor {
+	public:
+		void process(const Request &_request, LocalServer *_localServer) {
+			Response response;
+			_localServer->respond(_request, response);
+		}
+	};
+
 } // namespace dmc
 
 using namespace Poco::Net;
