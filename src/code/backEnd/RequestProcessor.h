bb2b960e61ae1aabc6b43f164e0b57116d4cd50e
Bardo91 <19Bardo91@gmail.com>
2015-07-06 12:22:25 +0200

    Defined RequestProcessor interface.

---
+++ src/code/backEnd/RequestProcessor.h
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Dmc Node
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+#ifndef _DMCNODE_CODE_FRONTEND_REQUESTPROCESSOR_H_
+#define _DMCNODE_CODE_FRONTEND_REQUESTPROCESSOR_H_
+
+namespace dmc {
+	// Forward Declarations
+	class Request;
+	class LocalServer;
+
+	/// Base interface of backend request processors.
+	class RequestProcessor {
+	public:
+		/// Process received dmc request and response properlly to the server.
+		virtual void process(const Request &_request, const LocalServer *_localServer) = 0;
+
+
+	};	//	 class RequestProcessor
+}	//	namespace RequestProcessor
+
+#endif	//	_DMCNODE_CODE_FRONTEND_REQUESTPROCESSOR_H_
