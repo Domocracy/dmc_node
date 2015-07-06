ff508187cab311eb85750a0a9ba59eb1e2bc70f3
Bardo91 <19Bardo91@gmail.com>
2015-07-06 14:56:43 +0200

    Defined Request interface and members

--- src/code/frontEnd/Request.h
+++ src/code/frontEnd/Request.h
@@ -4,12 +4,20 @@
 #ifndef _DMCNODE_CODE_FRONTEND_REQUEST_H_
 #define _DMCNODE_CODE_FRONTEND_REQUEST_H_
 
-namespace dmc {
+#include <cjson/json.h>
 
+namespace dmc {
 	class Request {
 	public:
 		unsigned id() const;
 		std::string url() const;
+		cjson::Json body() const;
+
+	private:
+		unsigned	mId;
+		std::string mUrl;
+		cjson::Json mJson;
+
 	};
 
 }	//	namespace dmc
