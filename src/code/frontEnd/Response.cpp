5e48b6c5cfb51be769655266dd4a7ceb86dfb59b
technik <cjtech90@gmail.com>
2015-07-07 11:53:16 +0200

    Implemented dmc::Response

---
+++ src/code/frontEnd/Response.cpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Dmc Node
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "Response.h"
+#include <sstream>
+
+using namespace std;
+
+namespace dmc {
+
+	//------------------------------------------------------------------------------------------------------------------
+	Response::Response(const cjson::Json& _body) {
+		mContent = _body.serialize();
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	Response Response::ok() {
+		return Response(std::string(R"({"ok":true})"));
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	Response Response::invalidRequest() {
+		return error(ErrorCode::invalidRequest, "Invalid request");
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	void Response::serialize(std::ostream& _os) const {
+		_os << mContent;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	Response Response::error(ErrorCode _code, const std::string& _message) {
+		stringstream ss;
+		ss << R"({"ok":false, "error"{
+				"code":)" << unsigned(_code) << R"(,"message":")" << _message << "\"}}";
+		return Response(ss.str());
+	}
+
+
+
+}	//	namespace dmc
