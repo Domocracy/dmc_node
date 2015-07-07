0c292fb4ec62970fb0d88d80b528fe97750c2ce7
technik <cjtech90@gmail.com>
2015-07-07 10:55:03 +0200

    Derived mock classes from HTTPServerRequest and HTTPServerResponse for HTTPTranslator unit test.

--- test/unitTest/frontEnd/httpTranslator/test.cpp
+++ test/unitTest/frontEnd/httpTranslator/test.cpp
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------------------------------
-// local server unit test
+// http translator unit test
 //----------------------------------------------------------------------------------------------------------------------
 #include <cassert>
 #include <string>
@@ -10,6 +10,56 @@
 #include <Poco/Net/HTTPServerRequest.h>
 #include <Poco/Net/HTTPServerResponse.h>
 #include <sstream>
+
+class TestHttpRequest : public Poco::Net::HTTPServerRequest {
+public:
+	stringstream mStream;
+	SocketAddress mAddress;
+	HTTPServerParams *mParams;
+	HTTPServerResponse* mResponse;
+	// Inherited via HTTPServerRequest
+	virtual std::istream & stream() override { return mStream; }
+	virtual bool expectContinue() const override { return false; }
+	virtual const SocketAddress & clientAddress() const override { return mAddress; }
+	virtual const SocketAddress & serverAddress() const override { return mAddress;	}
+	virtual const HTTPServerParams & serverParams() const override { return *mParams; }
+	virtual HTTPServerResponse & response() const override { return *mResponse; }
+
+};
+
+class TestHttpResponse : public Poco::Net::HTTPServerResponse {
+public:
+	unsigned mSent = 0;
+	std::stringstream mSS;
+	// Inherited via HTTPServerResponse
+	virtual void sendContinue() override {return;}
+
+	// Possible ways to send
+	virtual std::ostream & send() override
+	{
+		assert(!sent());
+		mSent++;
+		return mSS;
+	}
+
+	virtual void sendBuffer(const void * pBuffer, std::size_t length) override 
+	{ 
+		assert(!sent());
+		mSent++;
+		mSS << reinterpret_cast<const char*>(pBuffer);
+		return;
+	}
+
+	virtual void sendFile(const std::string & path, const std::string & mediaType) override { assert(false); return;}
+	virtual void redirect(const std::string & uri, HTTPStatus status = HTTP_FOUND) override { assert(false); return;}
+	virtual void requireAuthentication(const std::string & realm) override { assert(false); return;}
+
+	virtual bool sent() const override
+	{
+		return mSent > 0;
+	}
+
+};
 
 namespace dmc {
 	// --- Mock
@@ -39,7 +89,7 @@ int main(int, const char**) {
 	// Empty response
 	Response empty = Response::ok();
 	HTTPTranslator t1;
-	HTTPResponse resp;
+	TestHttpResponse resp;
 	assert(t1.translate(empty, resp));
 	ostringstream oss;
 	resp.write(oss);
@@ -48,7 +98,7 @@ int main(int, const char**) {
 	// Response with body
 	Response ok = Response::ok("a"); // Simple answer
 	HTTPTranslator t2;
-	HTTPResponse resp2;
+	TestHttpResponse resp2;
 	assert(t2.translate(ok, resp2));
 	ostringstream oss2;
 	resp2.write(oss2);
@@ -56,7 +106,7 @@ int main(int, const char**) {
 	assert(resp.getStatus() == HTTPResponse::HTTP_OK);
 	// --- http -> dmc
 	// Complete request
-	HTTPRequest req;
+	TestHttpRequest req;
 	req.setURI("/a");
 	req.read(stringstream(string("{3}")));
 	HTTPTranslator a;
@@ -65,13 +115,13 @@ int main(int, const char**) {
 	assert(dmcReq.url() == "/a");
 	assert(int(dmcReq.body()) == 3);
 	// Ill-formed request with no url HTTPRequest req;
-	HTTPRequest req2;
+	TestHttpRequest req2;
 	req2.setURI("");
 	HTTPTranslator b;
 	Request dmcReq2(2);
 	assert(!b.translate(req2, dmcReq2)); // Il-formed request must fail to translate
 	// Ill-formed request with invalid json;
-	HTTPRequest req3;
+	TestHttpRequest req3;
 	req3.setURI("/b");
 	req3.read(stringstream(string("meh!")));
 	HTTPTranslator c;
