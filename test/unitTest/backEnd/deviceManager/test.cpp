dfacc2a9f2b47abe510412e4bc3b85f431081553
technik <cjtech90@gmail.com>
2015-07-08 13:39:16 +0200

    Implemented mock constructor to DeviceFactory.

--- test/unitTest/backEnd/deviceManager/test.cpp
+++ test/unitTest/backEnd/deviceManager/test.cpp
@@ -5,6 +5,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <cassert>
 #include <backEnd/DeviceManager.h>
+#include <backEnd/DeviceFactory.h>
 #include <backEnd/Device.h>
 
 namespace dmc {
@@ -19,12 +20,26 @@ namespace dmc {
 	};
 
 	TestDevice gD0(0);
+
+	// Mock Device factory
+	DeviceFactory* gFactory;
+	DeviceFactory::DeviceFactory() {}
+	void DeviceFactory::init() { gFactory = new DeviceFactory; }
+	DeviceFactory* DeviceFactory::get() { return gFactory; }
+	Device* DeviceFactory::load(unsigned _id) {
+		if(_id == 0)
+			return & gD0;
+		else
+			return nullptr;
+	}
+
 }	//	namespace dmc
 
 using namespace dmc;
 
 int main(int, const char**) {
 	// All tests
+	DeviceFactory::init();
 	DeviceManager mgr;
 	Device* d0 = mgr.device(0); // This one exists
 	assert(d0 == &gD0);
