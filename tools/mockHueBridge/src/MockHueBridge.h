3a5baffd83ed77a19840e2e6ad2e89597f1d8ead
technik <cjtech90@gmail.com>
2015-07-09 11:22:29 +0200

    MockHueBridge dummy application.

--- tools/mockHueBridge/src/MockHueBridge.h
+++ tools/mockHueBridge/src/MockHueBridge.h
@@ -5,7 +5,11 @@
 #define _MOCKHUEBRIDGE_MOCKHUEBRIDGE_H_
 
 class MockHueBridge {
-	//
+public:
+	MockHueBridge();
+	bool listen(unsigned _port);
+
+private:
 }; 
 
 #endif // _MOCKHUEBRIDGE_MOCKHUEBRIDGE_H_
--- tools/mockHueBridge/src/main.cpp
+++ tools/mockHueBridge/src/main.cpp
@@ -2,7 +2,14 @@
 // dmc_node
 //----------------------------------------------------------------------------------------------------------------------
 #include "MockHueBridge.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int, const char**) {
-	//
+	MockHueBridge bridge;
+	bridge.listen(5023);
+	std::string s;
+	cin >> s;
 }
