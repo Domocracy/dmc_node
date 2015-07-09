69a3455043789fe8560cb0260f51358cddee1c2a
joscormir <joscormir@gmail.com>
2015-07-09 11:27:20 +0200

    Finished Hue Driver interface. Prepared for revision.

--- src/code/backEnd/deviceDrivers/HueDriver.h
+++ src/code/backEnd/deviceDrivers/HueDriver.h
@@ -8,10 +8,43 @@
 #ifndef _DMCNODE_CODE_FRONTEND_HUEDRIVER_H_
 #define _DMCNODE_CODE_FRONTEND_HUEDRIVER_H_
 
-namespace dmc{
+#include <ostream>
+#include "cjson\json.h"
 
+namespace dmc{ 
+	namespace hue {
+	
+		class HueDriver{
+		public:
+			enum class State{
+				connected,
+				connecting,
+				disconnected
 
+			};
 
+			//Singleton
+
+			static HueDriver*	get();
+			static void			init();
+
+			State state() const{ return mState; }
+
+			bool putData	(const std::string& _url, const cjson::Json& _data, std::ostream& _errorInfo);
+			bool getData	(const std::string& _url, cjson::Json& _data, std::ostream& _errorInfo);
+			bool postData	(const std::string& _url, const cjson::Json& _data, std::ostream& _errorInfo);
+			bool deleteData	(const std::string& _url, const cjson::Json& _data, std::ostream& _errorInfo);
+
+		private:
+
+			State mState;
+			HueDriver(const cjson::Json&);
+
+			static HueDriver* sHueDriver;
+
+		};
+	
+	} //namespace hue
 } //namespace dmc
 
 
