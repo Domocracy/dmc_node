456d256172b67e8837bd17d67464cf55f819350f
Bardo91 <19Bardo91@gmail.com>
2015-07-14 14:41:17 +0200

    Added description of HueDevice methods

--- src/code/backEnd/devices/HueDevice.h
+++ src/code/backEnd/devices/HueDevice.h
@@ -16,7 +16,14 @@ namespace dmc{
 			
 		class HueDevice : public Device {
 		public:
+			/// Hue Device constructor.
+			/// \param _id Device id (dmc id).
+			/// \param _data necesary data to instance HueDevice (666 not defined yet)
 			HueDevice(unsigned _id, const cjson::Json &_data);
+			/// Process command received.
+			/// \param _command Json as string that contains "method", "command" and "urlAp" (Appendix to be added to url).
+			///			For example: {"method":"PUT","command":{"on":true},"urlAp":"/state"}.
+			/// \param _info additional information
 			CmdResult process(const std::string& command, std::ostream& info);
 		private:
 			bool parseCommand(const std::string& command);
