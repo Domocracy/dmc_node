d0f8041513412945e4d05b41077232a9b886f5c6
joscormir <joscormir@gmail.com>
2015-07-17 14:08:16 +0200

    Testing response to make all lights turn off

--- tools/hueAttack/main.cpp
+++ tools/hueAttack/main.cpp
@@ -6,7 +6,9 @@
 
 #include <iostream>
 #include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
 #include <Poco/Net/HTTPClientSession.h>
+#include <cjson/json.h>
 
 using namespace Poco::Net;
 
@@ -15,7 +17,20 @@ int main(int, const char**) {
 	std::cout << "Starting attack" << std::endl;
 	
 	HTTPClientSession session("10.100.3.211");
-	HTTPRequest cmdOff;
+	HTTPRequest lightsList;
+	lightsList.setMethod("GET");
+	lightsList.setURI("/api/attack/lights");
+	session.sendRequest(lightsList);
+
+	HTTPResponse responseLightsList;
+	std::istream& resp =  session.receiveResponse(responseLightsList);
+	std::istreambuf_iterator<char> stmBuff;
+	std::string responseString(std::istreambuf_iterator<char>(resp), stmBuff);
+
+	std::cout << responseString << std::endl;
+
+	system("PAUSE");
+	/*HTTPRequest cmdOff;
 	cmdOff.setMethod("PUT");
 	cmdOff.setURI("/api/attack/lights/2/state");
 	std::string body = "{\n\t\"on\": false\n}";
@@ -25,6 +40,6 @@ int main(int, const char**) {
 		session.sendRequest(cmdOff) << body;
 		Sleep(500);
 	}
-	
+	*/
 
 }
